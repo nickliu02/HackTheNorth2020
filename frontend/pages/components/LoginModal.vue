71897cb037d65e31a8551b6e02eaa30ed8538c6f
Nick Liu <nickliu02@gmail.com>
2021-01-16 17:20:23 -0500

    lol

--- frontend/pages/components/LoginModal.vue
+++ frontend/pages/components/LoginModal.vue
@@ -16,7 +16,7 @@
                                             <v-text-field v-model="loginUsername" label="Username" required></v-text-field>
                                         </v-col>
                                         <v-col cols="12">
-                                            <v-text-field v-model="loginPassword"  name="input-10-1" label="Password"></v-text-field>
+                                            <v-text-field v-model="loginPassword" type="password" name="input-10-1" label="Password"></v-text-field>
                                         </v-col>
                                         <v-col class="d-flex" cols="12" sm="6" xsm="12">
                                         </v-col>
