e6dcf0ad5f92de014ddaf5376ffba8387e972d17
Zhehai Zhang <zhehaizhang1@gmail.com>
2021-01-16 07:29:16 -0500

    login page and auth stuff

--- frontend/middleware/auth.js
+++ frontend/middleware/auth.js
@@ -1,7 +1,10 @@
-export default
-function ({store,redirect}) {
-  if (!store.state.auth.authenticated) {
-    // return redirect('/login');
+export default function ({store,route,redirect}) {
+  console.log("This is ",route.name);
+  if (route.name === "index"){
     return redirect('/Home');
+  }
+  if (!store.state.auth.authenticated && route.name!="Home") {
+    // return redirect('/login');
+    return redirect('/Login');
   }
 }
---
+++ frontend/pages/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <LoginModal v-bind:dialog="true"></LoginModal>
+  </v-app>
+
+</template>
+
+<script>
+import LoginModal from './components/LoginModal';
+export default {
+  name: "Login",
+
+  components: {
+    LoginModal
+  },
+  data: () => ({
+
+  }),
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
