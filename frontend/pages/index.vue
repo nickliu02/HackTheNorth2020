60f55dc05e0447758483cb8751a2e0bf4fc4ea0c
Nick Liu <nickliu02@gmail.com>
2021-01-15 21:55:58 -0500

    nuxt

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,20 +1,12 @@
 <template>
-  <v-app>
     
-  </v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
-
   components: {
-  
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    
+  }
+}
 </script>
